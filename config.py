eb3770414e3316c8061f1bcf40d9540705e58b59
yegor_note <yegor6741@gmail.com>
2022-06-22 13:22:14 +0700

    config universality

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 # Config file for renderer
 
-SERVICE_HOST = '172.16.204.52'
+SERVICE_HOST = ''
 SERVICE_PORT = 8000
 
-DATABASE_HOST = '172.16.204.176'
-DATABASE_NAME = 'uc'
-DATABASE_USER = 'uc'
-DATABASE_PASSWORD = 'ucPassword'
+DATABASE_HOST = ''
+DATABASE_NAME = ''
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
 
 MINIMUM_BACKGROUND_BRIGHTNESS = 160 # Minimal brightness to switch text color from black into while
 CACHE_DIR = 'cached_photo'
 CACHE_EXPIRATION_TIME = 3600*5 # In seconds (5 hour)
 
-FONT_LOCATION = '/usr/share/fonts/truetype/roboto/unhinted/RobotoTTF/Roboto-Medium.ttf'
+FONT_LOCATION = '' #'/usr/share/fonts/truetype/roboto/unhinted/RobotoTTF/Roboto-Medium.ttf'
